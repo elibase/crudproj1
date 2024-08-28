8750e14a4d30eb63453f3b0b5f3e424361741b73
eli <elibask@yahoo.ca>
2024-08-27 20:30:46 -0400

    created a home page

--- App/main.py
+++ App/main.py
@@ -1,3 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="./App/templates")
+
+#read
+@app.get("/", response_class=HTMLResponse)
+async def home_page(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+@app.get("students/id/{student_id}")
+async def get_id(student_id: int):
+    return {"id": student_id}
