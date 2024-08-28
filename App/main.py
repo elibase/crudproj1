b3f5863d0d23e29bce012ea78d07b9b8ad2d4348
eli <elibask@yahoo.ca>
2024-08-28 15:07:55 -0400

    page that adds new student

--- App/main.py
+++ App/main.py
@@ -6,12 +6,17 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 templates = Jinja2Templates(directory="./App/templates")
+app.mount("/templates", StaticFiles(directory="./App/templates"), name="template")
 
 #read
 @app.get("/", response_class=HTMLResponse)
 async def home_page(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
-@app.get("students/id/{student_id}")
+@app.get("/addnew", response_class=HTMLResponse)
+async def addnew(request: Request):
+    return templates.TemplateResponse(request=request, name="addnew.html")
+
+@app.get("/students/id/{student_id}")
 async def get_id(student_id: int):
     return {"id": student_id}
